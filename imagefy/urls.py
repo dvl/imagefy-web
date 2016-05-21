dcc76bc2b2c5e3e447bf4baa6f14b272ba7f021a
André Luiz <contato@xdvl.info>
2016-05-21 05:43:30 -0300

    remove index

--- imagefy/urls.py
+++ imagefy/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.views.generic import TemplateView
 
 from rest_framework.authtoken import views as rest_views
 
@@ -8,7 +7,6 @@ from imagefy.core.views import FacebookLogin
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='base.html'), name='index'),
     url(r'^api/v1/', include('rest_auth.urls')),
     url(r'^api/v1/registration/', include('rest_auth.registration.urls')),
     url(r'^api/v1/registration/facebook/$', FacebookLogin.as_view(), name='fb_login'),
