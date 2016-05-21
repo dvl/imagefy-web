e469c421b539a26895387ac4760fb88a61dda58b
André Luiz <contato@xdvl.info>
2016-05-21 13:07:49 -0300

    padroniza as urls

--- imagefy/urls.py
+++ imagefy/urls.py
@@ -10,11 +10,13 @@ from imagefy.core.views import FacebookLogin
 urlpatterns = [
     url(r'^api/v1/', include(router.urls)),
 
-    url(r'^rest-auth/', include('rest_auth.urls')),
-    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
-    url(r'^rest-auth/facebook/$', FacebookLogin.as_view(), name='fb_login'),
+    url(r'^api/v1/rest-auth/', include('rest_auth.urls')),
+    url(r'^api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
+    url(r'^api/v1/rest-auth/facebook/$', FacebookLogin.as_view(), name='fb_login'),
 
-    url(r'^accounts/', include('allauth.urls')),
+    url(r'^api/v1/accounts/', include('allauth.urls')),
+
+    url(r'^api/v1/token-auth/', rest_views.obtain_auth_token),
 
     url(r'^api/v1/docs/', include('rest_framework_swagger.urls')),
     url(r'^admin/', admin.site.urls),
