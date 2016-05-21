7cd5b99afb86cd8c2c9ff622622c0f791ac053df
André Luiz <contato@xdvl.info>
2016-05-21 20:15:12 -0300

    auth classes para drf

--- imagefy/settings.py
+++ imagefy/settings.py
@@ -293,6 +293,10 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     )
 }
 
