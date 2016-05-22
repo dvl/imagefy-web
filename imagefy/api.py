a2b8c17bfec6acc6f995e9c60d96428fbadb4726
André Luiz <contato@xdvl.info>
2016-05-21 22:50:14 -0300

    usa o default router para que seja gerada a home

--- imagefy/api.py
+++ imagefy/api.py
@@ -2,7 +2,7 @@ from rest_framework_extensions import routers
 
 from imagefy.wishes.views import OfferViewSet, WishViewSet
 
-router = routers.ExtendedSimpleRouter()
+router = routers.ExtendedDefaultRouter()
 
 wishes_router = router.register(r'wishes', WishViewSet, base_name='wish')
 wishes_router.register(r'offers', OfferViewSet, base_name='wishes-offer', parents_query_lookups=['wish__pk', 'offer'])
