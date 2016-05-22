2063e9d1339a69929617120adf7235bb05aaa3d4
André Luiz <contato@xdvl.info>
2016-05-22 14:05:33 -0300

    remove referencias a lib não usada

--- imagefy/wishes/views.py
+++ imagefy/wishes/views.py
@@ -1,12 +1,11 @@
 from rest_framework import viewsets
-from rest_framework_extensions import mixins
 
 from imagefy.profile.models import Profile
 from imagefy.wishes.models import Offer, Wish
 from imagefy.wishes.serializers import OfferSerializer, WishSerializer
 
 
-class WishViewSet(mixins.NestedViewSetMixin, viewsets.ModelViewSet):
+class WishViewSet(viewsets.ModelViewSet):
     queryset = Wish.objects.all()
     serializer_class = WishSerializer
 
@@ -22,7 +21,7 @@ class WishViewSet(mixins.NestedViewSetMixin, viewsets.ModelViewSet):
         serializer.save(owner=self.request.user)
 
 
-class OfferViewSet(mixins.NestedViewSetMixin, viewsets.ModelViewSet):
+class OfferViewSet(viewsets.ModelViewSet):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
 
